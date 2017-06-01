5c74e1fedae569772b9e1ee80e6862f6016189b3
shivamneeraj23 <shivamneeraj23@gmail.com>
2017-06-01 13:19:33 +0530

    MINOR UI FIXED(1)

--- public/assets/js/login-modal.js
+++ public/assets/js/login-modal.js
@@ -1,64 +1,52 @@
-$(document).ready(function(){
-    $('.modal-footer button').click(function(){
-		var button = $(this);
+$(document).ready(function() {
+    $('.modal-footer button').click(function() {
+        var button = $(this);
 
-		if ( button.attr("data-dismiss") != "modal" ){
-			var inputs = $('form input');
-			var title = $('.modal-title');
-			var progress = $('.progress');
-			var progressBar = $('.progress-bar');
+        if (button.attr("data-dismiss") != "modal") {
+            var inputs = $('form input');
+            var title = $('.modal-title');
+            var progress = $('.progress');
+            var progressBar = $('.progress-bar');
 
-			inputs.attr("disabled", "disabled");
+            inputs.attr("disabled", "disabled");
 
-			button.hide();
+            button.hide();
 
-			progress.show();
+            progress.show();
 
-			progressBar.animate({width : "100%"}, 100);
+            progressBar.animate({ width: "100%" }, 100);
 
-			progress.delay(1000)
-					.fadeOut(600);
+            progress.delay(1000)
+                .fadeOut(600);
 
-			button.text("Close")
-					.removeClass("btn-primary")
-					.addClass("btn-success")
-    				.blur()
-					.delay(1600)
-					.fadeIn(function(){
-						title.text("Log in is successful");
-						button.attr("data-dismiss", "modal");
-					});
-		}
-	});
+            button.text("Close")
+                .removeClass("btn-primary")
+                .addClass("btn-success")
+                .blur()
+                .delay(1600)
+                .fadeIn(function() {
+                    title.text("Log in is successful");
+                    button.attr("data-dismiss", "modal");
+                });
+        }
+    });
 
-	$('#myModal').on('hidden.bs.modal', function (e) {
-		var inputs = $('form input');
-		var title = $('.modal-title');
-		var progressBar = $('.progress-bar');
-		var button = $('.modal-footer button');
+    $('#myModal').on('hidden.bs.modal', function(e) {
+        var inputs = $('form input');
+        var title = $('.modal-title');
+        var progressBar = $('.progress-bar');
+        var button = $('.modal-footer button');
 
-		inputs.removeAttr("disabled");
+        inputs.removeAttr("disabled");
 
-		title.text("Log in");
+        title.text("Log in");
 
-		progressBar.css({ "width" : "0%" });
+        progressBar.css({ "width": "0%" });
 
-		button.removeClass("btn-success")
-				.addClass("btn-primary")
-				.text("Ok")
-				.removeAttr("data-dismiss");
-                
-	});
+        button.removeClass("btn-success")
+            .addClass("btn-primary")
+            .text("Ok")
+            .removeAttr("data-dismiss");
+
+    });
 });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
