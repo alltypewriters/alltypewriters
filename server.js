f9dc865067ea7819ba211963432aaa7bcac23ea6
Astha-24 <gangwalastha78@gmail.com>
2017-06-01 12:43:37 +0530

    UI Fixes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
 app.set('view engine', 'ejs'); // set up ejs for templating
-
+app.use(express.static(__dirname+'/public')); //public directory
 // required for passport
 app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
