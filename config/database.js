015d12a5ca2f1d0737886bed70d31bc2f54ded06
shivamneeraj23 <shivamneeraj23@gmail.com>
2017-06-01 09:56:13 +0530

    Auth Minor Fix

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://<admin>:<admin123>@ds157641.mlab.com:57641/alltypewriters' 
+    'url': 'mongodb://admin:admin123@ds157641.mlab.com:57641/alltypewriters'
 
 };
